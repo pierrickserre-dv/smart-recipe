d07e07403b19d8dffb670d852190b609179ba3c3
pierrickserre-dv <pierrick.serre@devoteam.com>
2026-03-24 10:17:14 +0100

    fix import main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from src.auth.dependencies import get_current_user
 from src.generation.schemas import RecipeRequest, RecipeResponse
-from src.generation.RecipeService import RecipeService
+from src.generation.RecipeService import RecipeAIService
 
 app = FastAPI()
 
@@ -26,7 +26,7 @@ def say_hello():
     return {"message": "Route publique"}
 
 
-recipe_service = RecipeService()
+recipe_service = RecipeAIService()
 
 @app.post("/generate", response_model=RecipeResponse)
 async def generate_recipe_endpoint(
