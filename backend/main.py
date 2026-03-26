06cbed6ccb86490fa7b98165543f44648e496bba
pierrickserre-dv <pierrick.serre@devoteam.com>
2026-03-26 15:41:26 +0100

    feat/firestore: FirestoreService et route backend pour save a recipe

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.dependencies import get_current_user
+from src.auth.schemas import User
+from src.generation.persistence import FirestoreService
 from src.generation.RecipeService import RecipeAIService
 from src.generation.schemas import RecipeRequest, RecipeResponse
 
@@ -15,6 +17,7 @@ app.add_middleware(
 )
 
 recipe_service = RecipeAIService()
+firestore = FirestoreService()
 
 
 @app.get("/")
@@ -37,4 +40,21 @@ async def generate_recipe_endpoint(
         raise HTTPException(
             status_code=500,
             detail=f"Error: The AI couldn't generate a recipe. {str(e)}",
+        )
+
+
+@app.post("/save")
+async def save_recipe(request: RecipeResponse, user: User = Depends(get_current_user)):
+    try:
+        recipe_id = await firestore.save_recipe_for_user(user.uid, request)
+        return {
+            "status": "success",
+            "message": "Recipe saved successfully",
+            "id": recipe_id,
+        }
+    except Exception as e:
+        print(f"DEBUG: Firestore Error: {e}")
+        raise HTTPException(
+            status_code=500,
+            detail="An error occurred while saving the recipe to the database.",
         )
---
+++ backend/src/generation/persistence.py
@@ -0,0 +1,31 @@
+from google.cloud import firestore
+
+from .schemas import RecipeResponse
+
+
+class FirestoreService:
+    def __init__(self):
+        self.db = firestore.Client()
+
+    async def save_recipe_for_user(self, user_id: str, recipe: RecipeResponse):
+        user_recipes_ref = (
+            self.db.collection("users").document(user_id).collection("recipes")
+        )
+
+        recipe_data = recipe.model_dump()
+
+        recipe_data["created_at"] = firestore.SERVER_TIMESTAMP
+
+        _, doc_ref = user_recipes_ref.add(recipe_data)
+        return doc_ref.id
+
+    async def delete_recipe_for_user(self, user_id: str, recipe_id: str):
+        doc_ref = (
+            self.db.collection("users")
+            .document(user_id)
+            .collection("recipes")
+            .document(recipe_id)
+        )
+
+        doc_ref.delete()
+        return doc_ref
